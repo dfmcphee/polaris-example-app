93570b5e0ed7025017cd17da1db1e2e806aaaeb0
Dominic McPhee <dominic.mcphee@shopify.com>
2019-12-10 11:29:39 -0500

    Updating app page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppProvider, Page} from '@shopify/polaris';
+import {AppProvider, SkeletonPage, SkeletonBodyText, SkeletonDisplayText, Card, TextContainer, Layout} from '@shopify/polaris';
 import en from '@shopify/polaris/locales/en.json';
 import "@shopify/polaris/styles.css";
 
@@ -14,7 +14,40 @@ export default function App() {
         theme={theme}
         i18n={en}
       >
-        <Page title="Product reviews" />
+        <SkeletonPage title="Product reviews" secondaryActions={2}>
+          <Layout>
+            <Layout.Section>
+              <Card sectioned>
+                <SkeletonBodyText />
+              </Card>
+              <Card sectioned>
+                <TextContainer>
+                  <SkeletonDisplayText size="small" />
+                  <SkeletonBodyText />
+                </TextContainer>
+              </Card>
+              <Card sectioned>
+                <TextContainer>
+                  <SkeletonDisplayText size="small" />
+                  <SkeletonBodyText />
+                </TextContainer>
+              </Card>
+            </Layout.Section>
+            <Layout.Section secondary>
+              <Card>
+                <Card.Section>
+                  <TextContainer>
+                    <SkeletonDisplayText size="small" />
+                    <SkeletonBodyText lines={2} />
+                  </TextContainer>
+                </Card.Section>
+                <Card.Section>
+                  <SkeletonBodyText lines={1} />
+                </Card.Section>
+              </Card>
+            </Layout.Section>
+          </Layout>
+        </SkeletonPage>
       </AppProvider>
   );
 }
